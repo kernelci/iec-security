c382c16769d412b0ea0245b45d4ec598ca9ec3e2
Bui Duc Thinh <thinh1.buiduc@toshiba.co.jp>
2022-10-05 14:29:29 +0700

    shellcheck: fix error SC2143
    SC2143: Use grep -q instead of comparing output with [ -n .. ].
    
    Signed-off-by: Bui Duc Thinh <thinh1.buiduc@toshiba.co.jp>

--- iec-security-tests/singlenode-testcases/TC_CR3.4-RE2_1/runTest.sh
+++ iec-security-tests/singlenode-testcases/TC_CR3.4-RE2_1/runTest.sh
@@ -36,7 +36,7 @@ preTest() {
     echo "report_url=stdout" >> "${AIDE_CONF_FILE}"
 
 	# Add the application file to aide integrity check
-	if [ ! $(grep -q "$TEST_FILE  VarFile"  ${AIDE_CONF_FILE}) ];then
+	if ! grep -q "$TEST_FILE  VarFile"  ${AIDE_CONF_FILE};then
 		echo "$TEST_FILE  VarFile" >> "${AIDE_CONF_FILE}"
 	fi
 
